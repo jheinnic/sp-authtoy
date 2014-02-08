0f40693a9fe18250be9dcc2fdf9962314c3dcd9d
John <jheinnic@Davids-MacBook-Pro.local>
2014-02-07 17:38:00 -0800

    Attempted autowired approach.  Still no luck.

--- src/main/java/info/jchein/app/sp/authtoy/spring/appcontext/AuthtoyConfiguration.java
+++ src/main/java/info/jchein/app/sp/authtoy/spring/appcontext/AuthtoyConfiguration.java
@@ -12,9 +12,9 @@ import org.restlet.resource.Finder;
 import org.restlet.security.ChallengeAuthenticator;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -26,23 +26,24 @@ import com.stormpath.sdk.client.Client;
 import com.stormpath.sdk.client.ClientBuilder;
 
 @Configuration
-@PropertySource( name="authtoyProperties", value={ "classpath:authtoy.properties" })
+@PropertySource( name="authtoyProperties", value={ "classpath:/info/jchein/authtoy.properties" })
 public class AuthtoyConfiguration {
-	private static final String SP_KEY_FILE_PATH = "authtoy.stormpath.keyFile";
+	private static final String SP_KEY_FILE_PATH = "${authtoy.stormpath.keyFile}";
 
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 		PropertySourcesPlaceholderConfigurer pspc =
 			    new PropertySourcesPlaceholderConfigurer();
 			  Resource[] resources = new ClassPathResource[ ]
-			    { new ClassPathResource( "authtoy.properties" ) };
+			    { new ClassPathResource( "/info/jchein/authtoy.properties" ) };
 			  pspc.setLocations( resources );
-			  pspc.setIgnoreUnresolvablePlaceholders( true );
+			  pspc.setIgnoreUnresolvablePlaceholders( false );
 			  return pspc;
 	}
  	
-	@Autowired
-	private Environment env;
+	// @Autowired
+	// @javax.annotation.Resource
+	// private Environment env;
 
 	/*
 	@Inject 
@@ -52,10 +53,22 @@ public class AuthtoyConfiguration {
 	}
 	*/
 	
-	@Bean(autowire=Autowire.BY_TYPE)
+    @Bean(autowire=Autowire.BY_TYPE)
+    SpringComponent authtoyComponent(final AuthtoyApplication authtoyApplication) {
+    	SpringComponent bean = new SpringComponent();
+    	
+    	bean.setDefaultTarget(authtoyApplication);
+    	
+    	return bean;
+    }
+	
+    @Value(SP_KEY_FILE_PATH) 
+    private String keyFilePath;
+    
+	@Bean
 	@Autowired
-	Client spClient(Environment env) {
-		final String keyFilePath = env.getProperty(SP_KEY_FILE_PATH);
+	Client spClient() {
+		// final String keyFilePath = env.getProperty(SP_KEY_FILE_PATH);
 		final ClientBuilder clientBuilder = new ClientBuilder();
 		final Client bean = 
 			clientBuilder.setApiKeyFileLocation(keyFilePath).build();
@@ -65,63 +78,65 @@ public class AuthtoyConfiguration {
 
 	@Bean(autowire=Autowire.BY_TYPE)
 	@Autowired
-	StormpathVerifier spVerifier(Client spClient) { // Environment env) {
+	StormpathVerifier spVerifier(
+		// final Client spClient
+	) { // Environment env) {
 		final StormpathVerifier bean = new StormpathVerifier();
 		
-		bean.setStormpathClient(spClient);
+		bean.setStormpathClient(
+			spClient()
+		);
 		
 		return bean;
 	}
-	
-    @Bean
-	SpringComponent authtoyComponent() {
-    	SpringComponent bean = new SpringComponent();
+    
+    @Bean(autowire=Autowire.BY_TYPE)
+    @Autowired
+	AuthtoyApplication authtoyApplication(
+		// final SpringBeanRouter router
+	) {
+    	final AuthtoyApplication bean = new AuthtoyApplication();
     	
-    	bean.setDefaultTarget(
-    		authtoyApplication()
+    	bean.setInboundRoot(
+    		router()
     	);
     	
     	return bean;
     }
     
+	@Bean(autowire=Autowire.BY_TYPE)
+	@Autowired
+	Finder finder(final AuthtoyApplication authtoyApplication) {
+		final Finder bean = 
+			new Finder(
+				authtoyApplication.getContext()
+			);
+
+		return bean;
+	}
+ 
+	@Bean(autowire=Autowire.BY_TYPE)
+	Finder helloFinder(final Finder finder) {
+		final Finder bean = finder.createFinder(AuthtoyResource.class);
+
+		return bean;
+	}
+	   
     @Bean
-	AuthtoyApplication authtoyApplication() {
-    	final AuthtoyApplication bean = new AuthtoyApplication();
-    	
-    	bean.setInboundRoot(router());
-    	
-    	return bean;
-    }
-    
-    @Bean
+    @Autowired
 	SpringBeanRouter router() {
     	final SpringBeanRouter bean = new SpringBeanRouter();
     	
     	return bean;
     }
-    
-	@Bean
-	Finder finder() {
-		final Finder bean = 
-			new Finder(
-				authtoyApplication().getContext()
-			);
 
-		return bean;
-	}
-
-	@Bean
-	Finder helloFinder() {
-		final Finder bean = 
-			finder().createFinder(AuthtoyResource.class);
-
-		return bean;
-	}
-
-	@Bean(name={"/hello"}, autowire=Autowire.BY_TYPE)
+	@Bean(name="/hello", autowire=Autowire.BY_TYPE)
 	@Scope("prototype")
-	@Autowired
-	ChallengeAuthenticator helloGuard(StormpathVerifier spVerifier) { // Environment env) {
+	ChallengeAuthenticator helloGuard(
+		//final AuthtoyApplication authtoyApplication,
+		//final StormpathVerifier spVerifier,
+		//final Finder finder
+	) { // Environment env) {
 		System.out.println("helloGuard");
 		
 		final ChallengeAuthenticator bean = 
@@ -131,24 +146,24 @@ public class AuthtoyConfiguration {
 				"dooRealm"
 			);
 		
-		bean.setVerifier(
-			spVerifier
-		);
+		// The generic finder can be auto-wired, but its HelloResource-specific augmentation
+		// cannot be because that would yield two auto-wire candidates for bean class Finder.
+		bean.setVerifier(spVerifier());
 		bean.setNext(
-			helloFinder()
+			helloFinder(finder())
 		);
 		
 		System.out.println("...returning");
 		return bean;
 	}
 	
-	@Bean(name={"/assets"}, autowire=Autowire.BY_TYPE)
+	@Bean(name="/assets", autowire=Autowire.BY_TYPE)
 	@Scope("prototype")
-	Directory assetsDirectory() {
+	Directory assetsDirectory(final AuthtoyApplication authtoyApplication) {
 		System.out.println("assetsDir");
 		Directory bean = 
 			new Directory(
-				authtoyApplication().getContext(),
+				authtoyApplication.getContext(),
 				"file:///d:/DevProj/Git/sp-authtoy/src/main/webapp/assets"
 			);
 		
