cd8b648a8574ba3bf22e7cde76ac5621096f0f14
John Heinnickel <jheinnic@hotmail.com>
2014-02-09 13:18:10 -0800

    Whatever.  Making the application a @Component.  Not relevant since component-scanning is not currently in use.

--- src/main/java/info/jchein/app/sp/authtoy/application/AuthtoyApplication.java
+++ src/main/java/info/jchein/app/sp/authtoy/application/AuthtoyApplication.java
@@ -1,7 +1,9 @@
 package info.jchein.app.sp.authtoy.application;
 
 import org.restlet.Application;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthtoyApplication extends Application {
 
 }
