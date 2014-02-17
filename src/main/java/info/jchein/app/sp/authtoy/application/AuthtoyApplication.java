ec36bf99d900bf39cb4c76be9225fe49b24d7319
John Heinnickel <jheinnic@hotmail.com>
2014-02-17 00:35:37 -0800

    Application configuration changes.  Currently non-functional again, but worth checkpointing.  The Application's Context, not the Component's, _is_ the right one to use after all, but it must be wired to a Virtual Host before being extracted--the allocation to the Virtual Host is what triggers instantiation of the Application's Context, a child of the Component's contet.  I guess that means the Virtual Host must also be wired to the Component.

--- src/main/java/info/jchein/app/sp/authtoy/application/AuthtoyApplication.java
+++ src/main/java/info/jchein/app/sp/authtoy/application/AuthtoyApplication.java
@@ -1,9 +1,17 @@
 package info.jchein.app.sp.authtoy.application;
 
 import org.restlet.Application;
+import org.restlet.Context;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthtoyApplication extends Application {
+	public AuthtoyApplication() {
+		super();
+	}
+
+	public AuthtoyApplication(Context context) {
+		super(context);
+	}
 
 }
--- src/main/java/info/jchein/app/sp/authtoy/spring/appcontext/AuthtoyConfiguration.java
+++ src/main/java/info/jchein/app/sp/authtoy/spring/appcontext/AuthtoyConfiguration.java
@@ -2,24 +2,25 @@ package info.jchein.app.sp.authtoy.spring.appcontext;
 
 import info.jchein.app.sp.authtoy.application.AuthtoyApplication;
 import info.jchein.app.sp.authtoy.resource.AuthtoyResource;
+import info.jchein.lib.restlet.ext.shiro.restlet.ShiroDelegationAuthenticator;
 import info.jchein.lib.restlet.ext.stormpath.StormpathVerifier;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-import org.restlet.data.ChallengeScheme;
+import org.restlet.Restlet;
 import org.restlet.ext.spring.SpringComponent;
 import org.restlet.ext.spring.SpringRouter;
 import org.restlet.resource.Directory;
 import org.restlet.resource.Finder;
 import org.restlet.routing.Router;
-import org.restlet.security.ChallengeAuthenticator;
-import org.springframework.beans.factory.annotation.Autowire;
+import org.restlet.routing.VirtualHost;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
@@ -31,16 +32,20 @@ import com.stormpath.sdk.client.ClientBuilder;
 @Configuration
 @PropertySource(name = "authtoyProperties", value = { "classpath:info/jchein/authtoy.properties" })
 public class AuthtoyConfiguration {
+	// Temporary placeholder--this should come from configuration.
+	private static final String AUTH_REALM_NAME = "Authtoy Realm";
+	private static final byte[] ENCRYPT_SECRET_KEY = new byte[] { 84, -24, 48, -25, 53, -43, -65, -4, 34, 9, 0, 0, 127, -128, 35, 95, -39, -52, 93, 92, 34, -4, -29};
+	
 	private static final String SP_KEY_FILE_PATH_NAME = "authtoy.stormpath.keyFile";
-	private static final String SP_KEY_FILE_PATH_EXPR = "${authtoy.stormpath.keyFile}";
 
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
 		return new PropertySourcesPlaceholderConfigurer();
 	}
 
-	@Value("${authtoy.stormpath.keyFile}")
-	private String keyFilePath;
+	// private static final String SP_KEY_FILE_PATH_EXPR = "${authtoy.stormpath.keyFile}";
+	// @Value(SP_KEY_FILE_PATH_EXPR)
+	// private String keyFilePath;
 
     @Autowired
     private Environment env;
@@ -49,125 +54,160 @@ public class AuthtoyConfiguration {
 		System.out.println("Constructed app context");
 	}
 
-	@Bean( name= {"authtoyComponent"} )
+	@Bean // (name= {"authtoyComponent"})
 	@Scope("application")
-	public SpringComponent authtoyComponent() {
+	public SpringComponent authtoyComponent( ) {
 		System.out.print("Constructing \"authtoyComponent\" bean" );
 		
-		final SpringComponent bean = new SpringComponent();
-		final AuthtoyApplication authtoyApplication = authtoyApplication();
+		SpringComponent bean = new SpringComponent();
 
-		final ArrayList<Object> clients = new ArrayList<Object>();
+		final ArrayList<Object> clients = new ArrayList<Object>(3);
 		clients.add("FILE");
+		clients.add("CLAP");
 		bean.setClientsList(clients);
-		bean.setDefaultTarget(authtoyApplication);
-
-		// Wire the router to the application here to avoid circular dependencies
-		// between router and application that otherwise can only be resolved by 
-		// pushing the circular dependencies further along the route chain towards
-		// resource prototype leaves.
-		final Router router = router();
-		authtoyApplication.setInboundRoot(router);
-
+		
 		System.out.println( "..." );
 		return bean;
 	}
 
-	@Bean
-	@Scope("application")
-	Client spClient() {
-		String keyFilePathVal = env.getProperty("authtoy.stormpath.keyFile");
-		final ClientBuilder clientBuilder = new ClientBuilder();
-		final Client bean = clientBuilder.setApiKeyFileLocation(keyFilePathVal).build();
-
-		return bean;
-	}
-
-	@Bean
-	@Scope("application")
-	StormpathVerifier spVerifier() { // Environment env) {
-		final StormpathVerifier bean = new StormpathVerifier();
-
-		bean.setStormpathClient(spClient());
-
-		return bean;
-	}
-
-	@Bean
-	@Scope("application")
 	@Autowired
-	AuthtoyApplication authtoyApplication() {
-		final AuthtoyApplication bean = new AuthtoyApplication();
-
-		return bean;
-	}
-
-	@Bean
-	@Scope("application")
-	Finder finder() {
-		final Finder bean = new Finder(authtoyApplication().getContext());
-
-		return bean;
-	}
-
-	@Bean
-	@Scope("application")
-	Finder helloFinder() {
-		final Finder bean = finder().createFinder(AuthtoyResource.class);
-
-		return bean;
-	}
-
-	// Be explicit about this not being lazy since not instantiating it yields a 
-	// non-functional, but Component-attached Application object.
-	@Bean
+	private AuthtoyApplication authtoyApplication;
+	
+	@Bean // ( name= {"authtoyApplication"} )
 	@Lazy(false)
 	@Scope("application")
-	Router router() {
-		final AuthtoyApplication authtoyApplication = authtoyApplication();
-		final Router bean = 
-			new Router(authtoyApplication.getContext()
-		);
+	public AuthtoyApplication authtoyApplication() {
+		System.out.println("Constructing \"authtoyApplication\" bean");
+		final SpringComponent authtoyComponent = authtoyComponent();
+		final AuthtoyApplication bean =
+			new AuthtoyApplication(
+				authtoyComponent.getContext()
+			);
+		final VirtualHost host = new VirtualHost( authtoyComponent.getContext() );
+//		host.setHostPort("8080");
+//		host.setHostDomain("localhost");
+//		host.setResourceDomain("kropy");
+		host.setServerAddress("127.0.0.1");
+		host.setServerPort("8080");
 		
-		HashMap<String, Object> routes = new HashMap<String, Object>(5);
-		routes.put("/hello", helloGuard());
+		// Application wires both parent-to-child and child-to-parent relationships.  It must do the former, because its
+		// parent is a universal dependency.  It opts to do the latter, in order to invert convention.  The
+		// container makes itself available to receive applications and hosts, but an application is more
+		// intentional about how it is composed of parts.
+		bean.setInboundRoot(
+			authtoyRouter()
+		);
+		//authtoyComponent.setDefaultTarget(bean);
+		
+		// Read, modify, then re-insert host list.  Copy-on-write means get->add without a subsequent set is
+		// inadequate.
+		final List<VirtualHost> hostList = authtoyComponent.getHosts();
+		hostList.add(host);
+		authtoyComponent.setHosts(hostList);
+		host.attach(bean);
+		
+		System.out.println( "..." );
+		return bean;
+	}
+
+	@Bean(name= {"authtoyFinder"})
+	@Scope("application")
+	@Primary
+	Finder authtoyFinder() {
+		final Finder bean = 
+			new Finder(
+				authtoyComponent().getContext()
+			);
+
+		return bean;
+	}
+
+	// TODO: Candidate factory element
+	/*
+	@Bean(autowire=Autowire.BY_TYPE)
+	@Scope("application")
+	Finder helloFinder(final Finder finder) {
+		final Finder bean = finder.createFinder(AuthtoyResource.class);
+
+		return bean;
+	}
+	*/
+
+	@Bean( name= {"authtoyRouter"} )
+	@Scope("application")
+	Router authtoyRouter(/*final @Routable Collection<Restlet> routePaths*/) {
+		final Router bean = 
+			new Router(
+				authtoyComponent().getContext()
+			);
+		
+		final HashMap<String, Object> routes = new HashMap<String, Object>(5);
+		routes.put("/hello", helloResourcePath());
 		routes.put("/assets", assetsDirectory());
+		
 		SpringRouter.setAttachments(bean, routes);
 
 		return bean;
 	}
 
-	@Bean(name = "/hello", autowire = Autowire.BY_NAME)
+	@Bean(name = {"helloResourcePath","/hello"})
+	@Lazy(false)
 	@Scope("application")
-	ChallengeAuthenticator helloGuard( ) {
-		System.out.println("helloGuard");
-
-		final ChallengeAuthenticator bean = new ChallengeAuthenticator(
-				authtoyApplication().getContext(), ChallengeScheme.HTTP_COOKIE,
-				"dooRealm");
+	Restlet helloResourcePath() {
+		/*final ShiroDelegationFilter loadContextBean = 
+			new ShiroDelegationFilter(
+				authtoyComponent().getContext()
+			);*/
+		final SpringComponent authtoyComponent = authtoyComponent();
+		final StormpathVerifier spVerifier = spVerifier();
+		final Finder helloFinder =
+				authtoyFinder().createFinder(AuthtoyResource.class);
+		final ShiroDelegationAuthenticator authBean = 
+			new ShiroDelegationAuthenticator(
+				authtoyComponent.getContext(), AUTH_REALM_NAME, ENCRYPT_SECRET_KEY
+			);
 
 		// The generic finder could be auto-wired, but its HelloResource-specific
 		// augmentation cannot because that would yield two auto-wire candidates for 
 		// bean class "Finder".
-		bean.setVerifier(spVerifier());
-		bean.setNext(helloFinder());
+		authBean.setVerifier(spVerifier);
+		authBean.setNext(helloFinder);
+		// loadContextBean.setNext(authBean);
+		
+		return authBean;
+	}
 
-		System.out.println("...returning");
+	@Bean(name = {"/assets","assetsDirectory"})
+	@Scope("application")
+	Directory assetsDirectory(/*final authtoyComponent() authtoyComponent()*/) {
+		final SpringComponent authtoyComponent = authtoyComponent();
+		final Directory bean = 
+			new Directory(
+				authtoyComponent.getContext(), "clap:/context/assets"
+			);
+
 		return bean;
 	}
 
-	@Bean(name = "/assets", autowire = Autowire.BY_TYPE)
+	@Bean(name= {"spVerifier"})
 	@Scope("application")
-	Directory assetsDirectory() {
-		System.out.println("assetsDir");
-		final Directory bean = 
-			new Directory(
-				authtoyApplication().getContext(),
-				"cpath:/assets"
-				// "file:///d:/Dropbox/sp-authtoy/src/main/webapp/assets"
-			);
+	StormpathVerifier spVerifier() { 
+		final StormpathVerifier bean = new StormpathVerifier();
 
-		System.out.println("...returning");
+		bean.setStormpathClient(
+			spClient()
+		);
+
+		return bean;
+	}
+	
+	@Bean(name= {"spClient"})
+	@Scope("application")
+	Client spClient() {
+		final String keyFilePathVal = env.getProperty(SP_KEY_FILE_PATH_NAME);
+		final ClientBuilder clientBuilder = new ClientBuilder();
+		final Client bean = clientBuilder.setApiKeyFileLocation(keyFilePathVal).build();
+
 		return bean;
 	}
 }
